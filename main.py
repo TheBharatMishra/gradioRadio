81bdf31830522124a3836f863f521791c2c22030
TheBharatMishra <mishrabm2004@gmail.com>
2023-10-25 03:30:29 +0530

    OK

---
+++ main.py
@@ -0,0 +1,25 @@
+import gradio as gr
+
+
+# def greet(name):
+#     return "Hello " + name + "!"
+
+
+def greet(name, is_morning, temperature):
+    salutation = "Good Morning" if is_morning else "Good Evening"
+    greeting = f"{salutation} {name}. It is {temperature} degrees today"
+    celsius = (temperature - 32) * 5 / 9
+    return greeting, round(celsius, 2)
+
+
+# text, image, audio, label
+demo = gr.Interface(
+    # fn=greet, inputs=gr.Textbox(lines=2, placeholder="Name here"), outputs="text"
+    fn=greet,
+    inputs=["text", "checkbox", gr.Slider(0, 100)],
+    outputs=[
+        "text",
+        "number",
+    ],
+)
+demo.launch()
